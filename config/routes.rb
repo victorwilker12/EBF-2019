ef4dd6b7efcbba528d9bc7c45c13490a6839575d
victor-wilker <wilkersantos125@gmail.com>
2019-06-17 15:33:47 -0300

    change template

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     sign_in: 'login', 
     sign_out: 'logout'
   }
-  #controller: { sessions: 'user/sessions' }, skip: [ :registration ]
+
   root to: 'backsystem#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
