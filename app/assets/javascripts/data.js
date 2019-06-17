ee0dc5bf59445b9c3b1f22f5847d2bee4075ab6e
victor-wilker <wilkersantos125@gmail.com>
2019-06-17 14:22:13 -0300

    import css in scss

---
+++ app/assets/javascripts/data.js
@@ -0,0 +1,15 @@
+
+//= require "js/lib/data-table/datatables.min"
+//= require "js/lib/data-table/dataTables.bootstrap.min"
+//= require "js/lib/data-table/dataTables.buttons.min"
+//= require "js/lib/data-table/buttons.bootstrap.min"
+//= require "js/lib/data-table/jszip.min"
+//= require "js/lib/data-table/vfs_fonts"
+//= require "js/lib/data-table/buttons.html5.min"
+//= require "js/lib/data-table/buttons.print.min"
+//= require "js/lib/data-table/buttons.colVis.min"
+//= require "js/init/datatables-init"
+
+$(document).ready(function() {
+    $('#bootstrap-data-table-export').DataTable();
+} );
