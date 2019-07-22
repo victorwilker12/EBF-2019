5f8ac9882dcf6ba53471a0f32d433c2ba2a49793
Victor Wilker <victor.wilker@aluno.uece.br>
2019-07-22 19:42:24 -0300

    add css view pdf

--- app/controllers/backsystem_controller.rb
+++ app/controllers/backsystem_controller.rb
@@ -5,7 +5,151 @@ class BacksystemController < ApplicationController
   def index
     @students = Student.all 
   end
- 
+  
+   def laranja1
+     @student= Student.all
+
+      respond_to do |format|
+        format.html
+        format.pdf do
+         render pdf: 'frequencia',
+         template: "backsystem/frec/laranja1.html.erb",
+         layout: "FREC/l1.html",
+          #orientation: 'Landscape',
+          page_size:    'A4'
+       end
+      end
+  end
+  def l2
+     @student= Student.all
+
+      respond_to do |format|
+        format.html
+        format.pdf do
+         render pdf: 'frequencia',
+         template: "backsystem/frec/l2.html.erb",
+         layout: "FREC/l2.html",
+          #orientation: 'Landscape',
+          page_size:    'A4'
+       end
+      end
+  end
+   def r1
+     @student= Student.all
+
+      respond_to do |format|
+        format.html
+        format.pdf do
+         render pdf: 'frequencia',
+         template: "backsystem/frec/r1.html.erb",
+         layout: "FREC/r1.html",
+          #orientation: 'Landscape',
+          page_size:    'A4'
+       end
+      end
+  end
+   def r2
+     @student= Student.all
+
+      respond_to do |format|
+        format.html
+        format.pdf do
+         render pdf: 'frequencia',
+         template: "backsystem/frec/r2.html.erb",
+         layout: "FREC/r2.html",
+          #orientation: 'Landscape',
+          page_size:    'A4'
+       end
+      end
+  end
+
+   def v1
+     @student= Student.all
+
+      respond_to do |format|
+        format.html
+        format.pdf do
+         render pdf: 'frequencia',
+         template: "backsystem/frec/v1.html.erb",
+         layout: "FREC/v1.html",
+          #orientation: 'Landscape',
+          page_size:    'A4'
+       end
+      end
+  end
+     def v2
+     @student= Student.all
+
+      respond_to do |format|
+        format.html
+        format.pdf do
+         render pdf: 'frequencia',
+         template: "backsystem/frec/v2.html.erb",
+         layout: "FREC/v2.html",
+          #orientation: 'Landscape',
+          page_size:    'A4'
+       end
+      end
+  end
+
+   def a1
+     @student= Student.all
+
+      respond_to do |format|
+        format.html
+        format.pdf do
+         render pdf: 'frequencia',
+         template: "backsystem/frec/a1.html.erb",
+         layout: "FREC/a1.html",
+          #orientation: 'Landscape',
+          page_size:    'A4'
+       end
+      end
+  end
+   def a2
+     @student= Student.all
+
+      respond_to do |format|
+        format.html
+        format.pdf do
+         render pdf: 'frequencia',
+         template: "backsystem/frec/a2.html.erb",
+         layout: "FREC/a2.html",
+          #orientation: 'Landscape',
+          page_size:    'A4'
+       end
+      end
+  end
+     def az
+     @student= Student.all
+
+      respond_to do |format|
+        format.html
+        format.pdf do
+         render pdf: 'frequencia',
+         template: "backsystem/frec/az.html.erb",
+         layout: "FREC/az.html",
+          #orientation: 'Landscape',
+          page_size:    'A4'
+       end
+      end
+  end
+
+    def ve
+     @student= Student.all
+
+      respond_to do |format|
+        format.html
+        format.pdf do
+         render pdf: 'frequencia',
+         template: "backsystem/frec/ve.html.erb",
+         layout: "FREC/ve.html",
+          #orientation: 'Landscape',
+          page_size:    'A4'
+       end
+      end
+  end
+
  
 
   private
